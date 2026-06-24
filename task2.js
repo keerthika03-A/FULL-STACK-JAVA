1cf3ddfc2f77f6d59921ce6be26a4ef2a301893c
keerthika03-A <keerthikaarumugam36@gmail.com>
2026-06-24 10:39:07 +0530

    task2

---
+++ task2.js
@@ -0,0 +1,11 @@
+//2. Check whether a number is positive, negative, or zero
+
+let number = -10;
+
+if (number > 0) {
+    console.log("Positive Number");
+} else if (number < 0) {
+    console.log("Negative Number");
+} else {
+    console.log("Zero");
+}
