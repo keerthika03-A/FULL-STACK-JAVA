50d993b1338566a618b52cea1e69c6ec1a645def
keerthika03-A <keerthikaarumugam36@gmail.com>
2026-06-24 10:38:35 +0530

    task1

---
+++ variabletask1.js
@@ -0,0 +1,25 @@
+//1. Write a JavaScript program to: • Declare variables using let and const • Perform addition, subtraction, multiplication, and division
+//variable declaration 
+let num1 = 20;
+let num2 = 10;
+
+const message = "Arithmetic Operations";
+
+// Addition
+let addition = num1 + num2;
+
+// Subtraction
+let subtraction = num1 - num2;
+
+// Multiplication
+let multiplication = num1 * num2;
+
+// Division
+let division = num1 / num2;
+
+// Output
+console.log(message);
+console.log("Addition =", addition);
+console.log("Subtraction =", subtraction);
+console.log("Multiplication =", multiplication);
+console.log("Division =", division);
